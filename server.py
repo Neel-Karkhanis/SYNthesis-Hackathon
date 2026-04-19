76f33b2951646b67ce569dd74a777dbbf46da33e
neelkarkhanis <neelkarkha71@gmail.com>
2026-04-19 12:21:02 -0700

    sdf

---
+++ server.py
@@ -0,0 +1,13 @@
+import http.server
+import ssl
+
+server_address = ('localhost', 4443)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print("Serving on https://localhost:4443")
+httpd.serve_forever()
